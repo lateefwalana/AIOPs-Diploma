6ebb0577a32001a1c68cc5caa6bf7c21a80d8c25
Muhammad Lateef <21155005+lateefwalana@users.noreply.github.com>
2024-06-07 02:02:25 +0100

    added notes for find command

--- linux_commands/awk.sh
+++ linux_commands/awk.sh
@@ -29,6 +29,67 @@ Wed-5-Jun
 awk 'BEGIN {print "Hey, my name is Muhammad Lateef"}'
 
 -----
+# run who command present data in tabular farm, add headers
+mroot:~$ who | awk 'BEGIN { OFS="\t"; print "Active", "Sessions"} {print $1,$4}'
+Active  Sessions
+mroot   21:43
+mroot   21:43
+mroot   22:27
+
+mroot:~$ awk -v name="Ali" 'BEGIN {print name}'
+Ali
+
+mroot:~$ name="Ali"
+mroot:~$ awk -v name=$name 'BEGIN{print name}'
+Ali
+
+mroot:~$ cat /etc/passwd | awk 'BEGIN {print "START"} /mroot/ {print $0} END {print "END"}'
+START
+mroot:x:1000:1000:mroot,,,:/home/mroot:/bin/bash
+END
+
+# for loop
+awk 'BEGIN {for (i=1; i<=10; i++) print "The numner is: ", i}'
+The numner is:  1
+The numner is:  2
+The numner is:  3
+The numner is:  4
+The numner is:  5
+The numner is:  6
+The numner is:  7
+The numner is:  8
+The numner is:  9
+The numner is:  10
+
+# while loop
+
+mroot:~$ awk 'BEGIN { i=1; while (i<=10) print "The number is: "i++ }'
+The number is: 1
+The number is: 2
+The number is: 3
+The number is: 4
+The number is: 5
+The number is: 6
+The number is: 7
+The number is: 8
+The number is: 9
+The number is: 10
+
+mroot:~$ awk 'BEGIN { i=1; while (i<=10) {print "The number is: ", i++ }}'
+The number is:  1
+The number is:  2
+The number is:  3
+The number is:  4
+The number is:  5
+The number is:  6
+The number is:  7
+The number is:  8
+The number is:  9
+The number is:  10
+
+
+
+
 
 
 
---
+++ linux_commands/find.sh
@@ -0,0 +1,6 @@
+# find command
+
+find path -name filename # This finds files 
+
+find path -type d -name directory name
+
